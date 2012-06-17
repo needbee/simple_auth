042222b58e7b402462aaa4f3cf32b467abccedc1
Josh Justice <josh@need-bee.com>
2012-06-17 08:14:26 -0400

    first commit

---
+++ lib/simple_auth.rb
@@ -0,0 +1,4 @@
+require 'simple_auth/user'
+
+module SimpleAuth
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '363b44b056c6c3e04e4736b14b905af56b02fce54d832e43745e0e226288eb49140ab5d6bd2cf7409e6688c0a58a4696e9acf15e172742f862457cf2fa133c0b'
